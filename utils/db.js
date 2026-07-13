6d7f68bf546e355c1b2eba7e8b83b14da1aa67c9
divyansh-panda <divy.panda@gmail.com>
2026-07-13 19:48:24 +0530

    addNew

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,6 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose")
 
-const dbcon =async () => {
+const dbcon = async () => {
     try {
         await mongoose.connect("mongodb+srv://jsaurabh334:jsaurabh334@cluster0.w1qui.mongodb.net/divyansh?appName=Cluster0")
         console.log("db connected")
@@ -8,4 +8,4 @@ const dbcon =async () => {
         console.log(error)
     }
 }
-module.exports=dbcon
+module.exports = dbcon
